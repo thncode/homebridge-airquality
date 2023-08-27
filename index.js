16e98d2f39f797bf024c802e776eaf3cf487fc47
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2023-08-27 18:34:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+var Accessory, Service, Characteristic, UUIDGen, FakeGatoHistoryService;
+const packageFile = require("./package.json");
+var hostname = os.hostname();
+
+module.exports = function(homebridge) {
+    if(!isConfig(homebridge.user.configPath(), "accessories", "AirQuality")) {
+        return;
+    }
+    
+    Accessory = homebridge.platformAccessory;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
+    FakeGatoHistoryService = require("fakegato-history")(homebridge);
+
+    homebridge.registerAccessory('homebridge-airquality', 'AirQuality', AirQuality);
+}
 
